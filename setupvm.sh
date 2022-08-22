3df4f900a10a4b5bf5299d272ddb3a55cb602f1e
KrisSandy <krishnasandeep.g@gmail.com>
2022-08-22 21:37:00 +0100

    Update setupvm.sh

--- setupvm.sh
+++ setupvm.sh
@@ -7,6 +7,7 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # install go 
+echo "Installing go..."
 wget "https://go.dev/dl/$gotar"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf $gotar
@@ -16,9 +17,12 @@ source ~/.profile
 
 
 # instal kind
+echo "Installing Kind..."
 go install sigs.k8s.io/kind@v0.14.0
+echo "export PATH=\$PATH:$(go env GOPATH)/bin" >> ~/.profile
 
 # install docker 
+echo "Installing docker..."
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 rm get-docker.sh
@@ -28,14 +32,17 @@ sudo usermod -aG docker $USER
 newgrp docker 
 
 # install az
+echo "Installing az cli..."
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az extension add --name connectedk8s
 
 # install kubectl
+echo "Installing kubectl..."
 snap install kubectl --classic
 sudo snap install kubectx --classic
 
 # install helm3
+echo "Installing helm3..."
 wget "https://get.helm.sh/$helmtar"
 tar -zxvf $helmtar
 sudo mv linux-arm/helm /usr/local/bin/helm
