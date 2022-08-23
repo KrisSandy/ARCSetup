d00f726830ffc560da9a1001bbb9b3c2863ecaf1
KrisSandy <krishnasandeep.g@gmail.com>
2022-08-23 12:33:16 +0100

    Update setupvm.sh

--- setupvm.sh
+++ setupvm.sh
@@ -17,24 +17,6 @@ echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
 echo "Installed go"
 sleep 3
 
-# install docker 
-echo "Installing docker..."
-sudo apt-get install ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh get-docker.sh
-# rm get-docker.sh
-# manage docker as a non-root user
-# sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-echo "Installed docker"
-sleep 3
-
 # install az
 echo "Installing az cli..."
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
